478a9b2a08270f674c6b8d6d107d77203a55ca84
danditw013 <triwbowo0110@gmail.com>
2025-06-24 14:17:47 +0700

    fix: add missing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="min-h-screen bg-green-50 text-gray-800 p-6">
       {/* Tidak ada tombol keranjang */}
